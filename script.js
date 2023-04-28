48987fc88b488ca7275adac82f3dd306f4c3a4b7
itsaladdin <aladdin.husseinali@elev.cybergymnasiet.se>
2023-04-28 10:10:06 +0200

    made all the sites and there style

---
+++ script.js
@@ -0,0 +1,55 @@
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// --------------------------------------------------------------------------------
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+//---------------------------------------------------------------------------------------
+
+function heart() {
+  document.getElementById("heart").style.Color = #e60605;
+
+  console.log("heart")
+}
+
+
+   // db = [] //databasen
+   function signIn() {
+    const emailAddress = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const tempObject =
+    {
+        emailOrUsername: emailAddress,
+        pass: password
+    }
+
+    db.push(tempObject);
+
+    for (let k in db) {
+        console.log(db[k]);
+    }
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
